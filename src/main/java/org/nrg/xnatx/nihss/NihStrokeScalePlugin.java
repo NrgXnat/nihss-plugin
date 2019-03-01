d058169fa930b1dd3cd3483ace07648bf4216d3f
Rick Herrick <jrherrick@wustl.edu>
2019-03-01 14:48:38 -0600

    Added plugin config class

---
+++ src/main/java/org/nrg/xnatx/nihss/NihStrokeScalePlugin.java
@@ -0,0 +1,15 @@
+package org.nrg.xnatx.nihss;
+
+import lombok.extern.slf4j.Slf4j;
+import org.nrg.framework.annotations.XnatDataModel;
+import org.nrg.framework.annotations.XnatPlugin;
+import org.nrg.xdat.om.XnatxNihstrokescaledata;
+
+@XnatPlugin(value = "xnatxNihssPlugin", name = "XNATX NIH Stroke Scale Plugin",
+            dataModels = {@XnatDataModel(value = XnatxNihstrokescaledata.SCHEMA_ELEMENT_NAME,
+                                         code = "NIHSS",
+                                         singular = "Stroke Scale",
+                                         plural = "Stroke Scales")})
+@Slf4j
+public class NihStrokeScalePlugin {
+}
