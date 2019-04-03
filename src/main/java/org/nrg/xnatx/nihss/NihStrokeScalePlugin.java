c2f2b86dc9f31d85f159c3129fc4654533321be2
Rick Herrick <jrherrick@wustl.edu>
2019-04-03 18:47:35 -0500

    Added logging configuration and updated to 1.7.6-SNAPSHOT

--- src/main/java/org/nrg/xnatx/nihss/NihStrokeScalePlugin.java
+++ src/main/java/org/nrg/xnatx/nihss/NihStrokeScalePlugin.java
@@ -5,7 +5,7 @@ import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
 import org.nrg.xdat.om.XnatxNihstrokescaledata;
 
-@XnatPlugin(value = "xnatxNihssPlugin", name = "XNATX NIH Stroke Scale Plugin",
+@XnatPlugin(value = "xnatxNihssPlugin", name = "XNATX NIH Stroke Scale Plugin", logConfigurationFile = "nihss-logback.xml",
             dataModels = {@XnatDataModel(value = XnatxNihstrokescaledata.SCHEMA_ELEMENT_NAME,
                                          code = "NIHSS",
                                          singular = "Stroke Scale",
