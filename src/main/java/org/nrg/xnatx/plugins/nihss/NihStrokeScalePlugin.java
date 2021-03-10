db33da7c950038ee297340fc2aec4dcdde2f1ec5
Matt Kelsey <kelseym@mir.wustl.edu>
2021-03-10 10:06:35 -0600

    Updated dependencies for compatibility with  XNAT 1.8.
    Updated to Java 1.8
    Moved plugin config to org.nrg.xnatx.plugins package

--- src/main/java/org/nrg/xnatx/plugins/nihss/NihStrokeScalePlugin.java
+++ src/main/java/org/nrg/xnatx/plugins/nihss/NihStrokeScalePlugin.java
@@ -1,10 +1,21 @@
-package org.nrg.xnatx.nihss;
+/*
+ * nihss-plugin: org.nrg.xnatx.plugins.nihss.NihStrokeScalePlugin
+ * XNAT http://www.xnat.org
+ * Copyright (c) 2005-2021, Washington University School of Medicine
+ * All Rights Reserved
+ *
+ * Released under the Simplified BSD.
+ */
+
+package org.nrg.xnatx.plugins.nihss;
 
 import lombok.extern.slf4j.Slf4j;
 import org.nrg.framework.annotations.XnatDataModel;
 import org.nrg.framework.annotations.XnatPlugin;
 import org.nrg.xdat.om.XnatxNihstrokescaledata;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @XnatPlugin(value = "xnatxNihssPlugin", name = "XNATX NIH Stroke Scale Plugin", logConfigurationFile = "nihss-logback.xml",
             dataModels = {@XnatDataModel(value = XnatxNihstrokescaledata.SCHEMA_ELEMENT_NAME,
                                          code = "NIHSS",
